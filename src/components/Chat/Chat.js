5c1f8717dff8f68488e6519a4bf5413d65c714dd
abdi mussa <abdimussa87@gmail.com>
2021-01-11 12:10:01 +0300

    Initial commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function Chat() {
+    return (
+        <div className='chat'>
+            <h4>Chat</h4>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/components/Join/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Join.css'
+function Join() {
+    return (
+        <div className='join'>
+            <h5>Join</h5>
+        </div>
+    )
+}
+
+export default Join
